0530f57e1ad324fc87626b47b8279ed44a3072af
Martin Kearn <martin.kearn@microsoft.com>
2023-02-13 16:24:33 +0000

    Port old code

---
+++ MartinKMe/Models/ShortcutsViewModels/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MartinKMe.Models.ShortcutsViewModels
+{
+    public class IndexViewModel
+    {
+        public List<IGrouping<string,Shortcut>> Groups { get; set; }
+
+        public string WallpaperUri { get; set; }
+    }
+}
